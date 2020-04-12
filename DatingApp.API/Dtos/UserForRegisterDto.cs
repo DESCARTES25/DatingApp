89e8d773c458adc467f44daa93ca96181cbddf4a
Descartin <descartes25@hotmail.com>
2020-04-12 17:51:38 +0200

    Authorization

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username {get;set;}
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must supply a password with at least 4 characters")]
+        public string Password {get;set;}
+    }
+}
